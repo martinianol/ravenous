6f101cf908e685fbf3f1d512bc317983076582a1
Mars <martinianol@outlook.com>
2021-09-27 20:24:28 -0300

    Yelp utility to contact API

---
+++ src/util/Yelp.js
@@ -0,0 +1,34 @@
+const clientID = 'ufYr6HthwR4AX';
+const apiKey = 'V4rZej2x3qCkYB213POfPAi0hugqOigDQj3WbtPh2yZCIf4zci-b0MnffHI1SwNdiGtjY6FzbBcmI3xExIzKSw-Yo84o3Yr6to2MHXftmTC3BUMA77UkcB5CB0FSYXYx';
+
+const Yelp = {
+  search: (term, location, sortBy) => {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    })
+      .then(response => { response.json() })
+      .then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => {
+            return {
+              id: business.id,
+              name: business.name,
+              imageSrc: business.imageSrc,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
+            }
+          })
+        }
+
+      })
+  }
+}
+
+export default Yelp;
