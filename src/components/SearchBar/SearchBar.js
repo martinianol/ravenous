940b8919051e267f9128944c807b6b5272187c66
Mars <martinianol@outlook.com>
2021-09-26 21:18:07 -0300

    Part III completed

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,7 +13,8 @@ class SearchBar extends React.Component {
     }
     this.handleTermChange = this.handleTermChange.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
-
+    this.handleSortByChange = this.handleSortByChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
 
     this.sortByOptions = {
       'Best Match': 'best_match',
@@ -42,6 +43,11 @@ class SearchBar extends React.Component {
 
   };
 
+  handleSearch(event) {
+    event.preventDefault()
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+  };
+
 
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map(sortByOption => {
@@ -68,7 +74,7 @@ class SearchBar extends React.Component {
           <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-          <a>Let's Go</a>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     )
